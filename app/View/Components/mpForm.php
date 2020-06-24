d35cbf491b3b096cba7822c6213509806e69c206
Lucas Ventuala <ventualalucas@gmail.com>
2020-06-24 20:24:14 -0300

    nombre de clase en mayuscula

--- app/View/Components/mpForm.php
+++ app/View/Components/mpForm.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class mpForm extends Component
+class MpForm extends Component
 {
     public $categorias;
     public $unidad_medida;
